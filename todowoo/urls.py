ac8a15f05de0f79086f2772f1f0df667f6198417
Breland Dortch <dortchBreland@gmail.com>
2021-07-27 14:18:51 -0500

    current todos pages is created

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -19,6 +19,7 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', views.signupuser, name='signupuser')
+    path('signup/', views.signupuser, name='signupuser'),
+    path('current/', views.currenttodos, name='currenttodos')
     
 ]
