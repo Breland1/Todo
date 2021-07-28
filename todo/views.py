d5c67957738f27860b6919195f00034d6f1e25b1
Breland Dortch <dortchBreland@gmail.com>
2021-07-28 01:44:04 -0500

    login required

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -47,6 +48,7 @@ def logoutuser(request):
         return redirect('home')
 
 
+@login_required
 def createtodos(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': TodoForm()})
@@ -61,11 +63,19 @@ def createtodos(request):
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data passed in'})
 
 
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
 
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
+
+
+@login_required
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'GET':
@@ -80,6 +90,7 @@ def viewtodo(request, todo_pk):
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
 
 
+@login_required
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
@@ -88,6 +99,7 @@ def completetodo(request, todo_pk):
         return redirect('currenttodos')
 
 
+@login_required
 def deletetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
